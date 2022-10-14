59b7c0e4ff5a288c8b38933ae12273b94b961046
Andrew <andrewonulak@gmail.com>
2022-10-13 22:40:00 -0600

    commit tree b80e42ba03f609607d3519abe753580e03a01a13

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+
+ const returnFirstTwoDrivers = (driver) => [] = driver.slice(0,2)
+ const returnLastTwoDrivers = (driver) => [] = driver.slice(-2)
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+ function createFareMultiplier(argument){
+    return function(argument){
+        return (argument * argument)
+    }
+ }
+ const fareDoubler = (fare) => (fare * 2)
+ const fareTripler = (fare) => (fare * 3)
+
+ function selectDifferentDrivers(drivers, functions){
+    if (functions === returnFirstTwoDrivers){
+        return [] = drivers.slice(0,2)
+    } else {
+        return [] = drivers.slice(-2)
+    }
+ }
